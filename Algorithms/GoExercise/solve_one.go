bc3da42699ae9d627e1b9a278f8585ddcdfc16f1
dynastymasra <dynastymasra@gmail.com>
2015-09-29 16:14:30 +0700

    new

---
+++ Algorithms/GoExercise/solve_one.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func add(a uint32, b uint32) uint32 {
+  return (a+b)
+}
+
+func main()  {
+  var a, b, res uint32
+  fmt.Scanf("%v %v", &a, &b)
+  res = add(a, b)
+  fmt.Println(res)
+}
