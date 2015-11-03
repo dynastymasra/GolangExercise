8aaea6c851ccbbaa209eb8057fbe743540ac7676
dynastymasra <dynastymasra@gmail.com>
2015-11-03 18:46:15 +0700

    undone

--- Algorithms/Implementation/div_mod.go
+++ Algorithms/Implementation/div_mod.go
@@ -2,6 +2,63 @@ package main
 
 import "fmt"
 
+var result string
+
+func divisible(value int) string {
+
+  if value <= 2 {
+    data := "-1"
+    result = ""
+    return data
+  } else if value%3 == 0 {
+    for i:=0; i<value; i++ {
+      result += "5"
+    }
+    data := result
+    result = ""
+    return data
+  } else if value%5 == 0 {
+    for index := 0; index < value; index++ {
+      result += "3"
+    }
+    data := result
+    result = ""
+    return data
+  } else {
+    temp := value - 3
+    result += "555"
+    return result + divisible(temp)
+  }
+}
+
+func chose(v int) {
+  if v <= 10 {
+    fmt.Println(divisible(v))
+  } else {
+    sum(v)
+  }
+}
+
+func sum(v int) {
+  temp := v - 5
+
+  for i := temp; i > 0; i = i-5 {
+    if i%3 == 0 {
+      for index := 0; index < temp; index++ {
+          fmt.Print("5")
+      }
+      ex := v - i
+      for index := 0; index < ex; index++ {
+        fmt.Print("3")
+      }
+      fmt.Println("\n")
+      break
+    } else {
+      fmt.Println("-1")
+    }
+  }
+}
+
 func main() {
   var n int
   var v int
@@ -10,51 +67,10 @@ func main() {
   fmt.Scan(&n)
   for i := 0; i < n; i++ {
     v = 0
+    result = ""
     fmt.Println("Input Value :")
     fmt.Scan(&v)
 
-    m := v%3
-    n := v%5
-    if v <= 0 {
-      fmt.Println("-1")
-    } else if m == 0 {
-      for a := 0; a < v; a++ {
-        fmt.Print("3")
-      }
-      fmt.Println("")
-    } else if n == 0 {
-      for a := 0; a < v; a++ {
-        fmt.Print("5")
-      }
-      fmt.Println("")
-    } else if m%5 == 0 {
-      q := v-m
-      fmt.Println("m :", m)
-      fmt.Println("n :", n)
-      fmt.Println("q :", q)
-      for a := 0; a < q; a++ {
-        fmt.Print("3")
-      }
-      for a := 0; a < m; a++ {
-        fmt.Print("5")
-      }
-      fmt.Println("")
-    } else if n%3 == 0 {
-      q := v-m
-      fmt.Println("m :", m)
-      fmt.Println("n :", n)
-      fmt.Println("q :", q)
-      for a := 0; a < q; a++ {
-        fmt.Print("5")
-      }
-      for a := 0; a < n; a++ {
-        fmt.Print("3")
-      }
-      fmt.Println("")
-    } else {
-      fmt.Println("m :", m)
-      fmt.Println("n :", n)
-      fmt.Println("-1")
-    }
+    chose(v)
   }
 }
